91d22fc09dc98a6d7137568ee7ef42d7e06784f3
Andrés Ornelas <jose.ornelasa@alumnos.udg.mx>
2020-09-15 17:49:14 -0500

    Segundo commit

--- main.go
+++ main.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("\nSISTEMAS CONCURRENTES Y DISTRIBUIDOS - Actividad 02: Introducción a Git y Github")
+	fmt.Println("Este es un programa de prueba, con el objetivo de subirse a GitHub")
 }
