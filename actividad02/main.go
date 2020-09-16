a68cadd3bb3595413511a5185cbc88777f123bcc
Andrés Ornelas <jose.ornelasa@alumnos.udg.mx>
2020-09-15 21:09:16 -0500

    Carpeta

---
+++ actividad02/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\nSISTEMAS CONCURRENTES Y DISTRIBUIDOS - Actividad 02: Introducción a Git y Github")
+	fmt.Println("Este es un programa de prueba, con el objetivo de subirse a GitHub")
+}
